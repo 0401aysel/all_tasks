217a75cd0d1f58dc8fd27e60b93d5b4bf87a0bd7
Aysel <aysel@gmail.com>
2026-02-17 15:44:52 +0400

    JS class added for library system

--- library/scripts.js
+++ library/scripts.js
@@ -13,7 +13,9 @@ function getDate(date){
     const dateDate = `${dateYear}-${dateMonth}-${dateDay}`;
     return dateDate;
 }
+
 Book.prototype.borrowBook = function(){
+    console.log('Portotype used');
     if(this.isAvailable){
         const due = new Date();
         due.setDate(due.getDate() + 14); 
@@ -27,16 +29,61 @@ Book.prototype.borrowBook = function(){
 }
 
 Book.prototype.return =  function(){
+    console.log('Portotype used');
     this.isAvailable = true;
     this.dueDate = null;
     console.log('kitab ugurla geri qaytarildi.');
 }
 
 Book.prototype.isOverdue =  function(){
+    console.log('Portotype used');
     const today=new Date;
     const todayDate = getDate(today);
-    console.log(todayDate > this.dueDate);
+    console.log(todayDate < this.dueDate);
     console.log(`Cunki: kitabin qaytarilma tarixi:${this.dueDate} bugunku tarix ${todayDate}`)
 }
 
-let Mytest = new Book('book title', 'book author', true, '2026-05-01');
+let MyBook = new Book('book title', 'book author', true, '2026-05-01');
+
+
+// add class for library system instead of prototype ES6
+
+class Library{
+    constructor(title,author,isAvailable = true,dueDate = null){
+        this.title = title;
+        this.author = author; 
+        this.isAvailable = isAvailable;
+        this.dueDate = dueDate;
+    }
+
+    borrowBook(){
+        console.log('Class used');
+        if(this.isAvailable){
+            const due = new Date();
+            due.setDate(due.getDate() + 14); 
+
+            this.isAvailable = false;
+            this.dueDate = getDate(due);
+            console.log('Kitabi ugurla goturdunuz');
+        }else{
+            console.log('Kitab goturulub. Basqa secimlere baxin');
+        }
+    }
+
+    return(){
+        console.log('Class used');
+        this.isAvailable = true;
+        this.dueDate = null;
+        console.log('kitab ugurla geri qaytarildi.');
+    }
+
+    isOverdue(){
+        console.log('Class used');
+        const today=new Date;
+        const todayDate = getDate(today);
+        console.log(todayDate < this.dueDate);
+        console.log(`Cunki: kitabin qaytarilma tarixi:${this.dueDate} bugunku tarix ${todayDate}`)
+    }
+}
+
+const MyLibrary = new Library('book title', 'book author', true, '2026-01-04');
